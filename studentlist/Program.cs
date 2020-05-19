7704f1622d4f1d49051ec7e3eab484eae30c7a6e
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-19 12:09:18 +0900

    리스트 처리 특히 삭제할 때 조심하셈. 뒤에서부터 돌면서 지우는게 좋다

--- studentlist/Program.cs
+++ studentlist/Program.cs
@@ -18,11 +18,26 @@ namespace studentlist
             List<Teacher> list = new List<Teacher>();
             list.Add(new Teacher() { name = "함기훈", subject="뉴미디어콘텐츠제작" });
             list.Add(new Teacher() { name = "최규정", subject = "JSP" });
+            list.Add(new Teacher() { name = "박지우", subject = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "신혜정", subject = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "이대형", subject = "국어" });
 
-            foreach(var item in list)
+            for (int i = list.Count-1; i >= 0; i--)
             {
-                Console.WriteLine(item.name+""+item.subject);
+                if (list[i].subject == "정보컴퓨터")
+                {
+                    list.RemoveAt(i);
+                }
             }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.name+" : "+item.subject);
+                //if (item.subject == "정보컴퓨터")
+                //{
+                //    list.Remove(item);
+                //}
+            }
+
 
         }
     }
